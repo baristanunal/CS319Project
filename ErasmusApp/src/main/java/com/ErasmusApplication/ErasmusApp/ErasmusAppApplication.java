ddead22e1ca4c252870c4131815dd0e424d870a6
Yusuf <yusuf56senyuz@gmail.com>
2022-12-03 17:24:34 +0300

    Project is initialized again

---
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/ErasmusAppApplication.java
@@ -0,0 +1,13 @@
+package com.ErasmusApplication.ErasmusApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmusAppApplication.class, args);
+	}
+
+}
---
+++ ErasmusApp/src/test/java/com/ErasmusApplication/ErasmusApp/ErasmusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ErasmusApplication.ErasmusApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
