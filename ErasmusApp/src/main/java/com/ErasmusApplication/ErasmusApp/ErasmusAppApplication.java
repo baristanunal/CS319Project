a19f972d5b8313d010b7ccf20a5b0e5710276095
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-03 17:57:31 +0300

    Update ErasmusAppApplication.java

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/ErasmusAppApplication.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/ErasmusAppApplication.java
@@ -2,12 +2,18 @@ package com.ErasmusApplication.ErasmusApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ErasmusAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErasmusAppApplication.class, args);
 	}
-//
+	@RequestMapping("/h")
+	public String h(){
+		return  "hello";
+	}
 }
