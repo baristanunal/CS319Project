45b3681888dd318efb035bbd763821d3fd62fd86
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-07 22:23:54 +0300

    Class is put into comment since somehow it gives an error and I want to handle it later.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Dean.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Dean.java
@@ -1,11 +1,13 @@
-package com.ErasmusApplication.ErasmusApp.Models;
-
-import jakarta.persistence.Entity;
-import lombok.Data;
-
-@Data
-@Entity
-public class Dean extends UserClass{
-    // Properties
-    private String faculty;
-}
+//package com.ErasmusApplication.ErasmusApp.Models;
+//
+//import jakarta.persistence.Entity;
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//public class Dean extends UserClass{
+//    // Properties
+//    private String faculty;
+//
+//
+//}
