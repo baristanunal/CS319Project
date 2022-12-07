6733582daa5c836f1b173f8c630fa54d5011908f
Yusuf <yusuf56senyuz@gmail.com>
2022-12-07 21:33:13 +0300

    Formatting

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Services/StudentService.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Services/StudentService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
     ErasmusManager erasmusManager;
-//    LoginManager loginManager;
+    //LoginManager loginManager;
     //TODO  idk how this works
 
     @Autowired
