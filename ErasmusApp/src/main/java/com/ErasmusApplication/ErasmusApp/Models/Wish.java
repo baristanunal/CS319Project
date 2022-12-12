7ab59e75b386341ee4407119ac60b7bb142d29e7
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:12:28 +0300

    Wish class's properties added and relations is connected.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Wish.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Wish.java
@@ -1,7 +1,44 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
 public class Wish {
     // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String intent;
+    //TODO do smth for these
+    private String standing;
+    private String syllabus;
+
+    @JsonIgnore
+    @ManyToOne
+    private CourseWishList courseWishList;
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "wish_forms",
+            joinColumns = @JoinColumn(name = "wish_id"),
+            inverseJoinColumns = @JoinColumn(name = "form_id"))
+    private List<Form> forms;
+
+    public List<Form> getForms() {
+        return forms;
+    }
+
+    public void setForms(List<Form> forms) {
+        this.forms = forms;
+    }
+
 
     // Constructors
 
