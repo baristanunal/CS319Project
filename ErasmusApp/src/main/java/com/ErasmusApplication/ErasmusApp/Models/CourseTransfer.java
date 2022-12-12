27da681ffc5fe801f3212e7da8809df4365459f2
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:32:13 +0300

    CourseTransfer was updated for database. Relations were added.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseTransfer.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseTransfer.java
@@ -1,12 +1,24 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@Entity
+@Data
 public class CourseTransfer extends Form {
     // Properties
     private boolean signedByDean;
     private boolean signedByDepChair;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "courseTransfer")
+    private List<PassingGrade> passingGrades = new java.util.ArrayList<>();
 
-    private String[] passingGrades;
 
     // Constructors
 
+
+    //TODO Add method to Create Update Remove List object
 }
