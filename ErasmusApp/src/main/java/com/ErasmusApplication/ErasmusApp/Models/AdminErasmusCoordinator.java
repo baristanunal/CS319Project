184859b7adb755502eb03d6628044238b81c9566
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:41:17 +0300

    AdminErasmusCoordinator was updated for database.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/AdminErasmusCoordinator.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/AdminErasmusCoordinator.java
@@ -1,10 +1,19 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class AdminErasmusCoordinator extends UserClass{
     // Properties
     private String faculty;
 
-    public AdminErasmusCoordinator(String email, String firstName, String lastName, long schoolId) {
+    public AdminErasmusCoordinator(String email, String firstName, String lastName, long schoolId, String faculty) {
         super(email, firstName, lastName, schoolId);
+        this.faculty = faculty;
+    }
+    public AdminErasmusCoordinator() {
+
     }
 }
