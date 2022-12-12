317c10a207e6006fdd68042ede70b5ad0c9ff45c
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:28:00 +0300

    DepartmentErasmusCoordinator updated for database, Relations were added.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/DepartmentErasmusCoordinator.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/DepartmentErasmusCoordinator.java
@@ -1,19 +1,31 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class DepartmentErasmusCoordinator extends UserClass {
+
+    // Properties
+    @JsonIgnore
+    @ManyToOne( fetch = FetchType.LAZY)
+    private PlacementManager placementManager;
+
+
+
+// Constructors
+
+    public DepartmentErasmusCoordinator(PlacementManager placementManager) {
+        this.placementManager = placementManager;
+    }
     public DepartmentErasmusCoordinator(String email, String firstName, String lastName, long schoolId) {
         super(email, firstName, lastName, schoolId);
     }
-    // Properties
-    //private PlacementManager placementManager;
 
+    public DepartmentErasmusCoordinator() {
 
-    // Constructors
-
-
-
-    /**public DepartmentErasmusCoordinator(PlacementManager placementManager) {
-        this.placementManager = placementManager;
-    }*/
-
+    }
 }
