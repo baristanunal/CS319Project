9204d7532d76e6395441c0976a70d39eecec7750
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:19:57 +0300

    PreApproval class reviewed and TODO added

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/PreApproval.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/PreApproval.java
@@ -1,7 +1,13 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class PreApproval extends Form {
    // Properties
+   //TODO Change this to Date
    private String preApprovalDeadline;
 
    // Constructors
