f95fbe67e6bb3660d54b89e8ca4fe6362b3b6c49
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:24:35 +0300

    Form class was updated and its associations were added

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Form.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Form.java
@@ -1,10 +1,56 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Form {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
     // Properties
     private boolean isApproved;
     private boolean signedByDepCoordinator;
-    // private Wish[] wishes;
+
+    @JsonIgnore
+    @ManyToOne( fetch = FetchType.LAZY)
+    private Application application;
+
+    //TODO add relation
+    //TODO Add method to Create Update Remove List object
+    @JsonIgnore
+    @ManyToMany(mappedBy = "forms")
+    private List<Wish> wishes = new ArrayList<>();
+
+    public List<Wish> getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(List<Wish> wishes) {
+        this.wishes = wishes;
+    }
+
+//    private List<Wish> wishes;
+
+
+    public Application getApplication() {
+        return application;
+    }
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+
 
 }
