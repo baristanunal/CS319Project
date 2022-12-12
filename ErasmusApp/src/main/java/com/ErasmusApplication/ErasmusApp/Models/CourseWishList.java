66cca471970f88630ac776adf45806e822f6c0b1
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:30:15 +0300

    CourseWishList class was updated for database. Relations were added.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseWishList.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseWishList.java
@@ -1,9 +1,49 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
 public class CourseWishList {
     // Properties
-    private int totalCredit;
 
-    private Wish[] wishes;
-    //private PreApprovalManager preApprovalManager;
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @JsonIgnore
+    @OneToMany(
+            mappedBy = "courseWishList",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Wish> wishes;
+    private boolean isCompleted;
+    //TODO @Transient
+    private Double totalCredit;
+
+    @OneToOne(orphanRemoval = true)
+    @MapsId
+    private Application application;
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
