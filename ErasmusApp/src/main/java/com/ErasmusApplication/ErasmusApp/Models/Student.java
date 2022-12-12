b9783f5912cdc17298c01aea8dcc00fcc824567d
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:19:23 +0300

    update and remove methods for Application and Task attribute of Student class is added to ease update process with @Transactional

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Student.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Student.java
@@ -1,4 +1,6 @@
 package com.ErasmusApplication.ErasmusApp.Models;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
@@ -17,13 +19,21 @@ public class Student extends UserClass {
     private String birthDate;
     private String nationality;
     private String gender;
-    @JsonManagedReference
+    @JsonIgnore
     @OneToMany(
             mappedBy = "student",
             cascade = CascadeType.ALL,
             orphanRemoval = true
     )
     private List<Application> applications;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WaitingBin waitingBin;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PlacementTable placementTable;
 
 //    public Student(String email, String firstName, String lastName, long schoolId) {
 //        super(email, firstName, lastName, schoolId);
@@ -71,8 +81,17 @@ public class Student extends UserClass {
     }
 
 
+    //TODO unnecessary, if we want to delete or remove, we will iterate over List and we could handle it if it does not exist
+    public boolean checkExistenceOfApplication(Long applicationId){
+        Iterator<Application> iterator = applications.iterator();
+        while (iterator.hasNext()) {
+            if(iterator.next().getId() == applicationId){
+                return true;
+            }
+        }
+        return false;
+    }
     public Application getApplicationByType(String applicationType){
-        Application application;
         Iterator<Application> iterator = applications.iterator();
         while (iterator.hasNext()) {
             if(iterator.next().getApplicationType() == applicationType){
@@ -81,23 +100,54 @@ public class Student extends UserClass {
         }
         return null;
     }
-    public void removeApplication(Application application) {
-        applications.remove(application);
-    }
     public void addApplication(Application application){
         applications.add(application);
     }
-    public void removeApplicationByApplicationType(String applicationType) {
+
+    public boolean removeApplication(Application application) {
+        return applications.remove(application);
+    }
+    public boolean removeApplicationById(Long applicationId) {
+        Iterator<Application> iterator = applications.iterator();
+        while (iterator.hasNext()) {
+            if(iterator.next().getId() == applicationId){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean removeApplicationByApplicationType(String applicationType) {
         Iterator<Application> iterator = applications.iterator();
         while (iterator.hasNext()) {
             if(iterator.next().getApplicationType() == applicationType){
                 iterator.remove();
+                return true;
             }
         }
-
+        return false;
     }
 
+    //TODO check whether this works or not
+    public boolean updateApplicationByApplicationType(String applicationType, Application application) {
+        for (int i = 0; i < applications.size(); i++){
+            if(applications.get(i).getApplicationType() == applicationType){
+                applications.get(i).setAll(application);
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean updateApplicationByApplicationId(Long applicationId, Application application) {
+        for (int i = 0; i < applications.size(); i++){
+            if(applications.get(i).getId() == applicationId){
+                applications.get(i).setAll(application);
+                return true;
+            }
+        }
+        return false;
+    }
     //    @Id
 //    private Long studentId;
     // @Id
