49f404196bd0375904afa40396d121c59a1b5ef9
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:25:18 +0300

    minor changes for database

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/FacultyAdminCommittee.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/FacultyAdminCommittee.java
@@ -1,20 +1,23 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class FacultyAdminCommittee extends UserClass {
     public FacultyAdminCommittee(String email, String firstName, String lastName, long schoolId) {
         super(email, firstName, lastName, schoolId);
+    }
+
+    public FacultyAdminCommittee() {
+
     }
     // Properties
 
     // Constructors
 
     // Methods
-    public boolean approvePreApp(PreApproval preApproval){
-        return true; // to be implemented
-    }
 
-    public boolean approveCourseTransfer(CourseTransfer courseTransfer){
-        return true; // to be implemented
-    }
 
 }
