f20baee35de1ffc93127f80aa210b2b751231bbd
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:41:41 +0300

    Minor changes for mapping

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Controllers/TaskController.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Controllers/TaskController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/Task")
+
 public class TaskController {
     // TODO
 }
