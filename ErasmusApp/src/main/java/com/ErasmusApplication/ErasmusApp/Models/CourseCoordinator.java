0e4bfa33b237acbc9dc343987e920ba0565647a8
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:35:12 +0300

    Minor changes in CourseCoordinator class to connect it with the database.

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseCoordinator.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/CourseCoordinator.java
@@ -1,16 +1,46 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class CourseCoordinator extends UserClass{
     // Properties
     private String faculty;
     private String department;
     private String course;
 
-    public CourseCoordinator(String email, String firstName, String lastName, long schoolId) {
+    // Constructors
+
+    public CourseCoordinator(String email, String firstName, String lastName, long schoolId, String faculty, String department, String course) {
         super(email, firstName, lastName, schoolId);
+        this.faculty = faculty;
+        this.department = department;
+        this.course = course;
     }
 
-    // Constructors
+    public CourseCoordinator(String email, String firstName, String lastName, long schoolId, String faculty, String department) {
+        super(email, firstName, lastName, schoolId);
+        this.faculty = faculty;
+        this.department = department;
+    }
+
+    public CourseCoordinator(String faculty, String department) {
+        this.faculty = faculty;
+        this.department = department;
+    }
+
+    public CourseCoordinator(String faculty, String department, String course) {
+        this.faculty = faculty;
+        this.department = department;
+        this.course = course;
+    }
+
+    public CourseCoordinator() {
+
+    }
+
 
 
 }
