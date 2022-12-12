f31a6a39250d650f60fa142e310e8819412bc1ba
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:28:23 +0300

    DepartmentChairupdated for database,

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/DepartmentChair.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/DepartmentChair.java
@@ -1,11 +1,27 @@
 package com.ErasmusApplication.ErasmusApp.Models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class DepartmentChair extends UserClass {
     // Properties
     private String faculty;
     private String department;
 
-    public DepartmentChair(String email, String firstName, String lastName, long schoolId) {
+    public DepartmentChair(String email, String firstName, String lastName, long schoolId, String faculty, String department) {
         super(email, firstName, lastName, schoolId);
+        this.faculty = faculty;
+        this.department = department;
+    }
+
+    public DepartmentChair(String faculty, String department) {
+        this.faculty = faculty;
+        this.department = department;
+    }
+
+    public DepartmentChair() {
+
     }
 }
