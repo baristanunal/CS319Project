34080c335d5099d72964821d0170dc763f09a00b
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-12 16:29:44 +0300

    Dean class was updated for database

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Dean.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Models/Dean.java
@@ -1,13 +1,25 @@
-//package com.ErasmusApplication.ErasmusApp.Models;
-//
-//import jakarta.persistence.Entity;
-//import lombok.Data;
-//
-//@Data
-//@Entity
-//public class Dean extends UserClass{
-//    // Properties
-//    private String faculty;
-//
-//
-//}
+package com.ErasmusApplication.ErasmusApp.Models;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class Dean extends UserClass{
+    // Properties
+    private String faculty;
+
+
+    public Dean(String email, String firstName, String lastName, long schoolId, String faculty) {
+        super(email, firstName, lastName, schoolId);
+        this.faculty = faculty;
+    }
+
+    public Dean(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public Dean() {
+
+    }
+}
