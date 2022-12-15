135eab37d8b7fb7e59b0061969c376b83c58a7ea
baristanunal <baristunal45@gmail.com>
2022-12-15 21:55:41 +0300

    Added HostUniversityDepartment classes.

---
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Repositories/HostUniversityDepartmentRepository.java
@@ -0,0 +1,21 @@
+package com.ErasmusApplication.ErasmusApp.Repositories;
+
+import com.ErasmusApplication.ErasmusApp.Models.HostUniversityDepartment;
+import com.ErasmusApplication.ErasmusApp.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HostUniversityDepartmentRepository extends JpaRepository<HostUniversityDepartment, Long> {
+
+  @Query("select h from HostUniversityDepartment h where h.departmentName = ?1")
+  List<HostUniversityDepartment> findHostUniversityDepartmentsByDepartmentName(String name);
+
+
+  // HostUniversityDepartment findBy
+
+}
