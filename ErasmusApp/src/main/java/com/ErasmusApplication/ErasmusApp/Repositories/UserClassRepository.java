d2e90760627e1756e0c73f13b2e6ca3d5e7058ed
Ahmet_Sahin <sahmet@ug.bilkent.edu.tr>
2022-12-17 00:44:13 +0300

    Update UserClassRepository.java

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Repositories/UserClassRepository.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Repositories/UserClassRepository.java
@@ -14,7 +14,7 @@ public interface UserClassRepository extends JpaRepository<UserClass, Long> {
 
     @Query("select u from UserClass u where u.schoolId = ?1")
     Optional<UserClass> findBySchoolIdOpt(String schoolId);
-    
+
     @Query("select u from UserClass u where u.schoolId = ?1")
     UserClass findBySchoolId(String schoolId);
 
