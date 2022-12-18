19dd33a3d90d68cab5b0c33f3ab0e65c05badfb1
Yusuf <yusuf56senyuz@gmail.com>
2022-12-18 18:01:48 +0300

    Update PdfGenerationController.java

--- ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Controllers/PdfGenerationController.java
+++ ErasmusApp/src/main/java/com/ErasmusApplication/ErasmusApp/Controllers/PdfGenerationController.java
@@ -1,25 +1,24 @@
-package com.ErasmusApplication.ErasmusApp.Controllers;
-
-import com.ErasmusApplication.ErasmusApp.Services.PdfGenerationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-
-@RestController
-public class PdfGenerationController {
-    private final PdfGenerationService pdfGenerationService;
-
-    @Autowired
-    public PdfGenerationController(PdfGenerationService pdfGenerationService) {
-        this.pdfGenerationService = pdfGenerationService;
-    }
-    @GetMapping("/createPdf}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        return null;
-    }
-}
+//package com.ErasmusApplication.ErasmusApp.Controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.Resource;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//@RestController
+//public class PdfGenerationController {
+//    //private final PdfGenerationService pdfGenerationService;
+//
+//    @Autowired
+//    public PdfGenerationController(PdfGenerationService pdfGenerationService) {
+//        this.pdfGenerationService = pdfGenerationService;
+//    }
+//    @GetMapping("/createPdf}")
+//    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+//        return null;
+//    }
+//}
